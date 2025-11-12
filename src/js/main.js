e9027147981896f5250e4e73f48478ff6bb58368
Jadegomez <jade.gomez@studentfr.ch>
2025-11-12 14:41:44 +0100

    base family

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,29 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// family
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".family",
+      start: "top top",
+      end: "+=1000", // longueur du scroll
+      scrub: true,
+      pin: true, // bloque la section pendant l'anim
+    },
+  })
+  .to(".top", { y: "-100%", ease: "none" }, 0)
+  .to(".bottom", { y: "100%", ease: "none" }, 0)
+  .to(
+    ".family-case",
+    {
+      width: 793,
+      height: 557,
+      ease: "none",
+    },
+    0
+  );
+
 // scroll horizontal
 
 gsap.to(".screens", {
@@ -14,9 +37,22 @@ gsap.to(".screens", {
     scrub: true,
     markers: true,
   },
-  x: "-93%",
+  x: "-92.8%",
   ease: "sine.inOut",
 });
+
+// Animation des images
+tl.to(".top", { y: "-100%", ease: "none" }, 0)
+  .to(".bottom", { y: "100%", ease: "none" }, 0)
+  .to(
+    ".family-case",
+    {
+      width: 793,
+      height: 557,
+      ease: "none",
+    },
+    0
+  );
 
 // walking
 gsap.set([".carre-anim2", ".carre-anim3", ".carre-anim4"], {
